469162e6676c2fea76def6554f007ba0d255c5fc
Nolab <thibault.boutet@epita.fr>
2023-02-15 16:38:36 +0100

    serverize the app

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const { apiKey } = require("./key");
 const finnhub = require("finnhub");
+const express = require("express");
+const cors = require("cors");
 
-const getStockPrice = () => {
+// Create an express app
+const app = express();
+
+app.use(cors({ origin: "*" }));
+
+// Default endpoint
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// Endpoint to retrieve stock price
+app.get("/stockPrice", (req, res) => {
   const api_key = finnhub.ApiClient.instance.authentications["api_key"];
   api_key.apiKey = apiKey;
   const finnhubClient = new finnhub.DefaultApi();
-
   finnhubClient.quote("AAPL", (error, data, response) => {
-    console.log("Apple stock data:", data);
+    res.json(data);
   });
-};
+});
 
-getStockPrice();
+// Sart the express app
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}...`);
+});
